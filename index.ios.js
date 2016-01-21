d1d472d4e7018319bf1f360aaa111fb5e41fa1e8
Nader Dabit <dabit3@gmail.com>
2016-01-21 11:36:03 -0600

    Part3

--- index.ios.js
+++ index.ios.js
@@ -29,12 +29,13 @@ class toasts extends Component {
     }
   }
 
-  callToast() {
+  callToast(message, type) {
     if(this.state.modalShown) return
+    this.setToastType(message, type)
     this.setState({ modalShown: true })
     Animated.timing(
       this.animatedValue,
-      { 
+      {
         toValue: 0,
         duration: 350
       }).start(this.closeToast())
@@ -52,16 +53,46 @@ class toasts extends Component {
     }, 2000)
   }
 
+  setToastType(message='Success!', type='success') {
+    let color
+    if (type == 'error') color = 'red'
+    if (type == 'primary') color = '#2487DB'
+    if (type == 'warning') color = '#ec971f'
+    if (type == 'success') color = 'green'
+    this.setState({ toastColor: color, message: message })
+  }
+
   render() {
     return (
       <View>
         <View style={styles.container}>
+
           <View style={ styles.buttonContainer }>
-            <TouchableHighlight onPress={ () => this.callToast() } underlayColor="ddd" style={{ height:60, justifyContent: 'center', alignItems: 'center', backgroundColor: 'ededed', borderWidth: 1, borderColor: 'ddd' }}>
+              <TouchableHighlight onPress={ () => this.callToast('YOYOYO') } underlayColor="ddd" style={{ height:60, justifyContent: 'center', alignItems: 'center', backgroundColor: 'ededed', borderWidth: 1, borderColor: 'ddd' }}>
                 <Text>Open Success Toast</Text>
-            </TouchableHighlight>
-          </View>
+              </TouchableHighlight>
+            </View>
+
+            <View style={ styles.buttonContainer }>
+              <TouchableHighlight onPress={ () => this.callToast('Error toast called!', 'error') } underlayColor="ddd" style={{ height:60, justifyContent: 'center', alignItems: 'center', backgroundColor: 'ededed', borderWidth: 1, borderColor: 'ddd' }}>
+                <Text>Open Error Toast</Text>
+              </TouchableHighlight>
+            </View>
+
+            <View style={ styles.buttonContainer }>
+              <TouchableHighlight onPress={ () => this.callToast('Warning toast called!','warning') } underlayColor="ddd" style={{ height:60, justifyContent: 'center', alignItems: 'center', backgroundColor: 'ededed', borderWidth: 1, borderColor: 'ddd' }}>
+                <Text>Open Warning Toast</Text>
+              </TouchableHighlight>
+            </View>
+
+            <View style={ styles.buttonContainer }>
+              <TouchableHighlight onPress={ () => this.callToast('Primary toast called!', 'primary') } underlayColor="ddd" style={{ height:60, justifyContent: 'center', alignItems: 'center', backgroundColor: 'ededed', borderWidth: 1, borderColor: 'ddd' }}>
+                <Text>Open Primary Toast</Text>
+              </TouchableHighlight>
+            </View>
+
         </View>
+
         <Animated.View  style={{ transform: [{ translateY: this.animatedValue }], height: 70, backgroundColor: this.state.toastColor, position: 'absolute',left:0, top:0, right:0, justifyContent:  'center' }}>
           <Text style={{ marginLeft: 10,  color: 'white',  fontSize:16, fontWeight: 'bold' }}>
             { this.state.message }
